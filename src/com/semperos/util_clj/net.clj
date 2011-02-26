f953ab391ebdd62297b2066d09de114c421ae8ee
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-02-25 20:04:01 -0500

    Add support for ftp protocol to url-encode

--- src/com/semperos/util_clj/net.clj
+++ src/com/semperos/util_clj/net.clj
@@ -3,7 +3,7 @@
 (defn url-encode
   "Encode a string to URL-formatted string"
   [s]
-  (let [protocol (second (re-find #"^(http|https)://" s))
-        base (nth (re-find #"^(http|https)://([^/]*)/" s) 2)
-        path (nth (re-find #"^(http|https)://[^/]*(/.*?)$" s) 2)]
+  (let [protocol (second (re-find #"^(http|https|ftp)://" s))
+        base (nth (re-find #"^(http|https|ftp)://([^/]*)/" s) 2)
+        path (nth (re-find #"^(http|https|ftp)://[^/]*(/.*?)$" s) 2)]
     (.toString (java.net.URI. protocol base path nil))))
