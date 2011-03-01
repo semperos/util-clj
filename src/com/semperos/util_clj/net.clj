d499474c28a27a6702ac6e7b976495bb27d09037
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-01 13:18:40 -0500

    Change url-encode to uri-encode and remove arbitrary return of toString, just return the URI object

--- src/com/semperos/util_clj/net.clj
+++ src/com/semperos/util_clj/net.clj
@@ -1,9 +1,9 @@
 (ns com.semperos.util-clj.net)
 
-(defn url-encode
-  "Encode a string to URL-formatted string"
+(defn uri-encode
+  "Encode a string to a properly formatted URI"
   [s]
   (let [protocol (second (re-find #"^(http|https|ftp)://" s))
         base (nth (re-find #"^(http|https|ftp)://([^/]*)/" s) 2)
         path (nth (re-find #"^(http|https|ftp)://[^/]*(/.*?)$" s) 2)]
-    (.toString (java.net.URI. protocol base path nil))))
+    (java.net.URI. protocol base path nil)))
