794bae0f71d7a3f73f42ab37fc80e0b3d5c57ba0
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-01 13:16:38 -0500

    Add serialize-data function for writing Clojure data structures to file

--- src/com/semperos/util_clj/core.clj
+++ src/com/semperos/util_clj/core.clj
@@ -3,11 +3,20 @@
 ;; This is a collection of Clojure utilities that I want to keep handy.
 ;; Some of these are my own, some are snagged from elsewhere.
 
-(ns com.semperos.util-clj.core)
+(ns com.semperos.util-clj.core
+  (:use [clojure.contrib.duck-streams :only [with-out-writer]]))
 
-; Thanks to TJoC
+;; Thanks to TJoC
 (defmethod print-method clojure.lang.PersistentQueue
   [q w]
   (print-method '<- w)
   (print-method (seq q) w)
   (print-method '-< w))
+
+;; Thanks Clojuredocs.org user
+(defn serialize-data
+  "Print a data structure to a file so that we may read it in later."
+  [data-structure ^String filename]
+  (with-out-writer
+    (java.io.File. filename)
+    (binding [*print-dup* true] (prn data-structure))))
